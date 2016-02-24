df17dd845071d3d8c645977939e9d76ab75cd39f
Jeff Hajewski <jeff.hajewski@gmail.com>
2016-02-23 19:55:51 -0500

    Adds algorithms file with stubs for quicksort, partition, and randPartition

---
+++ algorithms.go
@@ -0,0 +1,24 @@
+// algorithms implements a number of common algorithm implementations
+// such as quicksort, mergesort, lower bound, upper bound, binary search, etc.
+package algorithms
+
+import "fmt"
+
+// partition returns the index of the partition value such that all elements
+// to the left of the index are less than the partition value and all
+// elements to the right or greater than the partitioning value
+func partition(v []int) int {
+
+}
+
+// similar to partition but instead of choosing the right most element as
+// the partition value, this function choose a value at random to be the
+// partitioning value
+func randPartition(v []int) int {
+
+}
+
+// Quicksort implementation using parition
+func Quicksort(v []int) {
+	
+}
