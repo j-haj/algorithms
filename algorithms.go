c5b59b931265136eb6249625143cea37acbcc3d8
Jeff Hajewski <jeff.hajewski@gmail.com>
2016-03-03 10:00:11 -0500

    Finishes initial working implementation of Sortable interface Quicksort. There is likely room for optimization here. Tests appear to be passing.

--- algorithms.go
+++ algorithms.go
@@ -5,59 +5,9 @@ package algorithms
 //package main
 
 import (
-	"fmt"
 	"math/rand"
 )
 
-/*
-type SortableSequence []int
-
-func (s SortableSequence) Len() int {
-	return len(s)
-}
-
-func (s SortableSequence) Less(i, j int) bool {
-	if s[i] < s[j] {
-		return true
-	}
-	return false
-}
-
-func (s SortableSequence) Greater(i, j int) bool {
-	if s[i] > s[j] {
-		return true
-	}
-	return false
-}
-
-func (s SortableSequence) Equal(i, j int) bool {
-	if s[i] == s[j] {
-		return true
-	}
-	return false
-}
-
-func (s SortableSequence) SubRange(i, j int) Sortable {
-	return s[i:j]
-}
-
-func (s SortableSequence) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func main() {
-	seq1 := SortableSequence{3, 7, 6, 5, 4, 2, 1}
-	seq2 := SortableSequence{3, 7, 6, 5, 4, 2, 1}
-	fmt.Printf("starting: %v\n", seq1)
-	Quicksort(seq1)
-	fmt.Printf("result: %v\n", seq1)
-
-	fmt.Println("\nTrying regular partition\n")
-	fmt.Printf("starting: %v\n", seq2)
-	StaticQuicksort(seq2)
-	fmt.Printf("result: %v\n", seq2)
-}
-*/
 // Sortable is an interface for data that is sortable. It works by defining
 // functions for getting the data length, determining the order of two data
 // items given their indices, and a swap functiont that swaps the items
@@ -67,7 +17,6 @@ type Sortable interface {
 	Less(i, j int) bool
 	Greater(i, j int) bool
 	Equal(i, j int) bool
-	SubRange(i, j int) Sortable
 	Swap(i, j int)
 }
 
@@ -114,10 +63,7 @@ func partition(v []int) int {
 
 // partition partitions a given data set that implements the sortable
 // interface. The initial element is chosen as the partition value
-func sortablePartition(s Sortable) int {
-	p := 0
-	i := 0
-	j := s.Len() - 1
+func sortablePartition(s Sortable, i, j, p int) int {
 	for {
 		for s.Less(i, p) {
 			i++
@@ -191,26 +137,23 @@ func sortableRandPartition(s Sortable) int {
 }
 
 // Quicksort implementation using parition
-func Quicksort(s Sortable) {
-	p := sortablePartition(s)
-	if s.Len() <= 2 {
+func Quicksort(s Sortable, i, j, p int) {
+	p = sortablePartition(s, i, j, p)
+	if j-i <= 2 {
 		return
 	}
-	fmt.Printf("p: %d\nv: %v\n", p, s)
-	fmt.Println("Starting left recursive call...")
-	if p > 0 {
-		Quicksort(s)
+	if p > i {
+		Quicksort(s, i, p, i)
 	}
-	fmt.Println("Starting right recursive call...")
-	if p < s.Len()-1 {
-		Quicksort(s.SubRange(p, s.Len()-1))
+	if p < j {
+		Quicksort(s, p+1, j, p+1)
 	}
 }
 
 // StaticQuicksort is quicksort using the first element as the partition
 func StaticQuicksort(v []int) {
 	p := partition(v)
-	if len(v) == 2 {
+	if len(v) <= 2 {
 		return
 	}
 	if p > 0 {
--- algorithms_test.go
+++ algorithms_test.go
@@ -29,13 +29,22 @@ func (s SortableSequence) Greater(i, j int) bool {
 
 func (s SortableSequence) Equal(i, j int) bool {
 	if s[i] == s[j] {
-		return false
+		return true
 	}
-	return true
+	return false
 }
 
 func (s SortableSequence) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
+}
+
+func isSorted(s Sortable) bool {
+	for i := 0; i < s.Len()-2; i++ {
+		if !s.Less(i, i+1) && !s.Equal(i, i+1) {
+			return false
+		}
+	}
+	return true
 }
 
 var seq1 = SortableSequence{3, 7, 6, 5, 4, 2, 1}
@@ -92,7 +101,7 @@ func TestRandPartition1(t *testing.T) {
 // ----------------------------------------------------------------------------
 // Sort Tests
 // ----------------------------------------------------------------------------
-func TestQuicksort(t *testing.T) {
+func TestStaticQuicksort1(t *testing.T) {
 	test := []int{6, 5, 1, 3, 9, 2, 8, 6, 6, 5}
 	expected := []int{1, 2, 3, 5, 5, 6, 6, 6, 8, 9}
 	StaticQuicksort(test)
@@ -100,5 +109,37 @@ func TestQuicksort(t *testing.T) {
 		if test[i] != expected[i] {
 			t.Fatalf("expected test[%d] = %d; got test[%d] = %d\n", i, test[i], i, expected[i])
 		}
+	}
+}
+
+func TestStaticQuicksort2(t *testing.T) {
+	test := []int{6, 6, 5, 5, 1, 1, 3, 3, 9, 9, 2, 2, 8, 8, 6, 6, 6, 5, 5}
+	expected := []int{1, 1, 2, 2, 3, 3, 5, 5, 5, 5, 6, 6, 6, 6, 6, 8, 8, 9, 9}
+	StaticQuicksort(test)
+	for i := 0; i < len(test); i++ {
+		if test[i] != expected[i] {
+			t.Fatalf("expected test[%d] = %d; got test[%d] = %d\n", i, test[i], i, expected[i])
+		}
+	}
+}
+
+func TestQuicksort1(t *testing.T) {
+	Quicksort(seq1, 0, seq1.Len()-1, 0)
+	if !isSorted(seq1) {
+		t.Fatalf("expected seq1 = %v; got seq1 = %v.", seq1, srt1)
+	}
+}
+
+func TestQuicksort2(t *testing.T) {
+	Quicksort(seq2, 0, seq2.Len()-1, 0)
+	if !isSorted(seq2) {
+		t.Fatalf("expected seq2 = %v; got seq2 = %v", seq2, srt2)
+	}
+}
+
+func TestQuicksort3(t *testing.T) {
+	Quicksort(seq3, 0, seq3.Len()-1, 0)
+	if !isSorted(seq3) {
+		t.Fatalf("expected seq3 = %v; got seq3 = %v.", seq3, srt3)
 	}
 }
